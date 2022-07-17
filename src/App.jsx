ec8a12b71ada652d0d45d958d84c817cfe834a18
Nacho Martin <martin.nacho.31@gmail.com>
2022-07-17 14:28:32 +0200

    Define views routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./views/Home";
+import Details from "./views/Details";
+import Admin from "./views/Admin";
+
 function App() {
   return (
-    <div className="App" />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/:city" exact element={<Details />} />
+        <Route path="/admin" exact element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
