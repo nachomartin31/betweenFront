b7b3ae693bb993078b87e12017844162f3009f3c
Nacho Martin <martin.nacho.31@gmail.com>
2022-07-18 15:33:39 +0200

    Implement combinereducers

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,14 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import speedSystemReducer from "./slices/speedUnit";
 import tempeperatureUnitReducer from "./slices/temperatureUnit";
-import currentDateReducer from "./currentDate";
+import currentDateReducer from "./slices/currentDate";
 
 // eslint-disable-next-line import/prefer-default-export
 export const store = configureStore({
-  reducer: {
+  reducer: combineReducers({
     speedSystem: speedSystemReducer,
     temperatureUnit: tempeperatureUnitReducer,
     currentDate: currentDateReducer
-  }
+  })
 });
