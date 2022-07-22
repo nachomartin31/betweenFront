ecbb39b6f131b8127acf423859ed65b8bbd6ab76
Nacho Martin <martin.nacho.31@gmail.com>
2022-07-22 11:27:10 +0200

    Delete useless slice

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,13 +2,11 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import speedSystemReducer from "./slices/speedUnit";
 import tempeperatureUnitReducer from "./slices/temperatureUnit";
-import currentDateReducer from "./slices/currentDate";
 
 // eslint-disable-next-line import/prefer-default-export
 export const store = configureStore({
   reducer: combineReducers({
     speedSystem: speedSystemReducer,
-    temperatureUnit: tempeperatureUnitReducer,
-    currentDate: currentDateReducer
+    temperatureUnit: tempeperatureUnitReducer
   })
 });
