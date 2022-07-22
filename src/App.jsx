f1f97ae600cb2a05eea0e56cfabc83d257e7c0c5
Nacho Martin <martin.nacho.31@gmail.com>
2022-07-22 11:28:33 +0200

    Add header component - Also, change path params into 'id'

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./views/Home";
 import Details from "./views/Details";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" exact element={<Home />} />
-        <Route path="/:city" exact element={<Details />} />
+        <Route path="/:id" exact element={<Details />} />
 
       </Routes>
     </BrowserRouter>
